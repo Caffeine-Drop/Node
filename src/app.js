5b1e9f965107f769d2405e5844865fded133125c
KIMMINJI <amber_lucy@naver.com>
2025-01-15 21:48:15 +0900

    .

--- src/app.js
+++ src/app.js
@@ -12,23 +12,6 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// MySQL 연결 설정
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-// MySQL 연결 테스트
-db.connect((err) => {
-  if (err) {
-    console.error('MySQL 연결 실패:', err);
-  } else {
-    console.log('MySQL에 연결되었습니다.');
-  }
-});
-
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
