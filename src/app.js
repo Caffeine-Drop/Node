b21a2bcdffb25b19aaba371f2c8c30b8dd369e37
KIMMINJI <amber_lucy@naver.com>
2025-01-15 20:38:30 +0900

    MySQL 설정

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2";
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +11,23 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// MySQL 연결 설정
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// MySQL 연결 테스트
+db.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결 실패:', err);
+  } else {
+    console.log('MySQL에 연결되었습니다.');
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
