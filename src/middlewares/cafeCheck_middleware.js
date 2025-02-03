bf3cec5b8547f12f772c36c8e913347b0fe589cd
plar8271 <plar8271@naver.com>
2025-02-03 09:56:13 +0900

    Clear feature/user branch contents

--- src/middlewares/cafeCheck_middleware.js
+++
@@ -1,9 +0,0 @@
-import { handleReadCafes } from '../controllers/cafe_controller.js';
-import { Router } from 'express';
-
-const router = Router();
-
-//특정 카페의 모든 관련 정보 불러오기
-router.get('/cafes/:cafe_id', handleReadCafes);
-
-export default router;
