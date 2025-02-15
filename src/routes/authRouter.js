3a53aae7a5a09afa38262e4be084e57580231690
plar8271 <plar8271@naver.com>
2025-02-15 18:59:56 +0900

    로그아웃 기능 추가, 그리고 그를 위한 prisma 수정

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,6 @@
+import { logout } from '../controllers/auth_controller.js';
 import express from 'express';
 import passport from 'passport';
-import { kakaoLogout, naverLogout } from '../auth.js'; // 로그아웃 함수 import
-
 
 const router = express.Router();
 
@@ -11,8 +10,12 @@ router.get("/callback/naver", passport.authenticate("naver", {
   failureRedirect: "/oauth2/login/naver",
   failureMessage: true,
 }), (req, res) => { 
-  const { accessToken, refreshToken } = req.user;
-  res.success({accessToken, refreshToken});
+  const { id, accessToken, refreshToken } = req.user;
+
+  req.session.accessToken = accessToken;  //로그아웃을 위해 세션에 토큰 저장
+  req.session.refreshToken = refreshToken;//DB 세션 테이블에서 data에 들어간다
+
+  res.success({id, accessToken, refreshToken});
 });
 
 // OAuth 로그인 라우팅 (카카오)
@@ -21,39 +24,16 @@ router.get("/callback/kakao", passport.authenticate("kakao", {
   failureRedirect: "/oauth2/login/kakao",
   failureMessage: true,
 }), (req, res) => {
-  const { accessToken, refreshToken } = req.user;
-  res.success({accessToken, refreshToken});
+  const { id, accessToken, refreshToken } = req.user;
+
+  req.session.accessToken = accessToken;  //로그아웃을 위해 세션에 토큰 저장
+  req.session.refreshToken = refreshToken;//DB 세션 테이블에서 data에 들어간다
+
+  res.success({id, accessToken, refreshToken});
 });
 
 // 로그아웃 라우팅
-router.get("/logout", async (req, res) => {
-  try {
-    if (req.user) {
-      const { kakaoAccessToken, naverAccessToken } = req.user;
-
-      // 카카오 로그아웃 처리
-      if (kakaoAccessToken) {
-        await kakaoLogout(kakaoAccessToken);
-      }
-
-      // 네이버 로그아웃 처리
-      if (naverAccessToken) {
-        await naverLogout(naverAccessToken);
-      }
-    }
-
-    // 세션 종료
-    req.logout((err) => {
-      if (err) {
-        return res.error(error, 500);
-      }
-      res.clearCookie('connect.sid');  // 세션 쿠키 삭제
-      res.success({ message: "로그아웃 완료" }, 200);
-    });
-  } catch (error) {
-    res.error(error, 500);
-  }
-});
+router.post('/logout', logout);
 
 export default router;
 
