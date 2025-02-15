cb47c67011c9f9dbfc7e6506dbbf8df3327763b7
noobcpuls <iop789007@gmail.com>
2025-02-15 18:31:54 +0900

    Feat: Prisma 업데이트, like와 user 라우팅 및 경로 수정

---
+++ src/middlewares/userRouter.js
@@ -0,0 +1,22 @@
+import express from "express";
+import * as userController from "../controllers/user_controller.js";
+
+const router = express.Router();
+
+// 닉네임 중복 체크 API
+router.get("/nickname/check/:user_id", userController.checkNicknameOverlap);
+
+// 닉네임 생성 API
+router.post("/nickname", userController.createNickname);
+
+// 닉네임 변경 API
+router.patch("/nickname", userController.changeNickname);
+
+// 유저 정보 조회 API
+router.get("/:user_id", userController.getUserInfo);
+
+export default router;
+
+/*
+app.js에 적용시 app.use('/users', userRouter);
+*/
