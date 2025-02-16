7326a296f555a5caceb464895d0831b4eb77a7db
plar8271 <plar8271@naver.com>
2025-02-16 11:48:06 +0900

    프로필 업로드 추가 but 아직 테스트 안함

--- src/repositories/user_repository.js
+++ src/repositories/user_repository.js
@@ -53,3 +53,16 @@ export const changeNickname = async (userId, newNickname) => {
     throw new InternalServerError('닉네임 변경 오류');
   }
 };
+
+//service 계층에서 전달받은 userId와 imageUrl를 데이터베이스에 저장하기 위한 함수
+export const updateUserProfileImage = async (userId, imageUrl) => {
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { user_id: userId },
+      data: { profile_image_url: imageUrl },
+    });
+    return updatedUser;
+  } catch (error) {
+    throw error;
+  }
+};
