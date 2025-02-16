2e7da8143c090a03c4d26b46fd0236342b6ecd25
KIMMINJI <amber_lucy@naver.com>
2025-02-16 16:50:23 +0900

    fix : 검색 완련 오류 수정

--- src/controllers/search_controller.js
+++ src/controllers/search_controller.js
@@ -7,7 +7,7 @@ const service = new SearchService(searchRepository);
 
 class SearchController {
     async searchByKeyword(req, res, next) {
-        const user_id = Number(req.user_id);
+        const user_id = String(req.user_id);
         const { keyword } = req.query;
         const lat = Number(req.query.lat);
         const lng = Number(req.query.lng);
@@ -76,7 +76,7 @@ class SearchController {
     }
 
     async getRecentTerms(req, res, next) {
-        const user_id = Number(req.user_id);
+        const user_id = String(req.user_id);
 
         if (!user_id || isNaN(user_id)) {
             return res.status(400).json({ message: '유저아이디는 필수이며 숫자여야 합니다.' });
@@ -102,7 +102,7 @@ class SearchController {
     }
 
     async deleteSearchTerm(req, res, next) {
-        const user_id = Number(req.user_id);
+        const user_id = String(req.user_id);
         const { keyword } = req.query;
 
         if (!user_id || isNaN(user_id)) {
@@ -133,7 +133,7 @@ class SearchController {
     }
     
     async deleteAllSearchTerms(req, res, next) {
-        const user_id = Number(req.user_id);
+        const user_id = String(req.user_id);
 
         if (!user_id || isNaN(user_id)) {
             return res.status(400).json({ message: '유저아이디는 필수이며 숫자여야 합니다.' });
