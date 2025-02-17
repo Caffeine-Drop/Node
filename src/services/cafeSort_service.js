969d6a7410568e9e5bb76722e0cde88981245031
taejun-J <sagecandy20@gmail.com>
2025-02-17 19:16:15 +0900

    feat:거리순 정렬기능 추가

--- src/services/cafeSort_service.js
+++ src/services/cafeSort_service.js
@@ -1,9 +1,25 @@
+// import { sortRepository } from '../repositories/cafeSort_repository.js';
+// import { cafeSortDto } from '../dtos/cafeSort_dto.js';
+
+// export class sortCafe {
+//   static async sortByEvalu(evaluation) {
+//     const cafeSort = await sortRepository.sortCafeByEvalu(evaluation);
+
+//     if (!Array.isArray(cafeSort)) {
+//       throw new Error('Evaluation result is not an array');
+//     }
+
+//     // CafeDto로 응답 데이터를 포맷 (배열 변환)
+//     return cafeSortDto.fromArray(cafeSort);
+//   }
+// }
+
 import { sortRepository } from '../repositories/cafeSort_repository.js';
 import { cafeSortDto } from '../dtos/cafeSort_dto.js';
 
 export class sortCafe {
-  static async sortByEvalu(evaluation) {
-    const cafeSort = await sortRepository.sortCafeByEvalu(evaluation);
+  static async sortByEvalu(evaluation, lat = null, lng = null) {
+    const cafeSort = await sortRepository.sortCafeByEvalu(evaluation, lat, lng);
 
     if (!Array.isArray(cafeSort)) {
       throw new Error('Evaluation result is not an array');
@@ -11,5 +27,16 @@ export class sortCafe {
 
     // CafeDto로 응답 데이터를 포맷 (배열 변환)
     return cafeSortDto.fromArray(cafeSort);
+  }
+
+  // 새로운 메서드 추가
+  static async sortByDistance(lat, lng) {
+    const cafeSort = await sortRepository.sortCafeByDistance(lat, lng);
+
+    if (!Array.isArray(cafeSort)) {
+      throw new Error('Distance sorting result is not an array');
+    }
+
+    return cafeSortDto.fromArray(cafeSort); // DTO로 변환해서 반환
   }
 }
