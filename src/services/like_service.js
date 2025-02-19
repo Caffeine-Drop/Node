dafd889f29331f3b7fd646f9e4d925095871f3f1
KIMMINJI <amber_lucy@naver.com>
2025-02-19 19:29:38 +0900

    feat : 좋아요한 카페 반환 API 구현

--- src/services/like_service.js
+++ src/services/like_service.js
@@ -1,4 +1,5 @@
 import { InternalServerError, NotFoundError } from "../error/error.js";
+import { CafeDto } from '../dtos/cafe_dto.js';
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
@@ -26,6 +27,42 @@ class LikeService {
 			console.error("카페 좋아요 하는 중 오류 발생:", error);
 
 			throw new InternalServerError("카페 좋아요 하는 중 오류가 발생했습니다.");
+		}
+	}
+
+	async myLikeCafe(userId) {
+		const user = await prisma.user.findFirst({ where: { user_id: userId } });
+		if (!user) {
+			throw new NotFoundError({ message: "유저가 존재하지 않습니다." });
+		}
+
+		try {
+			// 좋아요한 카페 가져오기
+			const like = await this.likeRepository.mycafe(userId);
+
+			// CafeDto 형식으로 변환
+			const cafeDtos = await Promise.all(
+				like.map(async (cafe) => {
+					const cafeDetails = await prisma.cafe.findUnique({
+						where: { cafe_id: cafe.cafe_id },
+						include: {
+							images: true,
+							menu_items: true,
+							reviews: true,
+							likes: true,
+							beans: true,
+							filters: true,
+							operating_hours: true,
+						},
+					});
+	
+					return new CafeDto(cafeDetails); // CafeDto로 변환하여 반환
+				})
+			);
+	
+			return cafeDtos;
+		} catch (error) {
+			throw new InternalServerError("좋아요 한 카페를 가져오는 중 오류가 발생했습니다.");
 		}
 	}
 }
