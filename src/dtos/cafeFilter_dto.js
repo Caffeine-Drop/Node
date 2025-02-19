352d5a3b0343397a91746391cd6d7a86a3596fa1
KIMMINJI <amber_lucy@naver.com>
2025-02-19 19:13:49 +0900

    fix : 카페 카테고리 필터링 추가

--- src/dtos/cafeFilter_dto.js
+++ src/dtos/cafeFilter_dto.js
@@ -1,8 +1,9 @@
 export class CafeFilterDto {
-  constructor(dayOfWeek, time, likes, rating) {
+  constructor(dayOfWeek, time, likes, rating, criteria) {
     this.dayOfWeek = dayOfWeek;
     this.time = time;
     this.likes = likes;
     this.rating = rating;
+    this.criteria = criteria;
   }
 }
