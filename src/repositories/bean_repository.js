3f7ef368c6faa8a1c7faba572d7c8913ab777d70
KIMMINJI <amber_lucy@naver.com>
2025-02-21 05:04:31 +0900

    fix : 데이터베이스 수정

--- src/repositories/bean_repository.js
+++ src/repositories/bean_repository.js
@@ -3,7 +3,7 @@ export const prisma = new PrismaClient();
 
 // 원두 정보 조회 함수 (원두아이디로 단일조회)
 export const getBean = async (beanId) => {
-  const bean = await prisma.Bean.findFirst({ where: { bean_id: beanId } });
+  const bean = await prisma.bean.findFirst({ where: { bean_id: beanId } });
   if (!bean) {
     return null;
   }
@@ -18,7 +18,7 @@ export const getBeansDetails = async (beanIds) => {
 
 // 원두아이디들 중 싱글오리진인 원두아이디만 반환하는 함수
 export const getSingleOriginID = async (beanIds) => {
-  const beans = await prisma.Bean.findMany({
+  const beans = await prisma.bean.findMany({
     where: { 
       bean_id: { in: beanIds },
       is_specialty: true 
@@ -34,7 +34,7 @@ export const getSingleOriginID = async (beanIds) => {
 
 // 원두들의 싱글오리진 정보를 가져오는 함수
 export const getSingleOriginDetail = async (beanIds) => {
-  const beans = await prisma.SingleOrigin.findMany({
+  const beans = await prisma.singleOrigin.findMany({
     where: { 
       bean_id: { in: beanIds }
     }
@@ -47,7 +47,7 @@ export const getSingleOriginDetail = async (beanIds) => {
 
 // 원두들과 관계된 태그 아이디를 가져오는 함수
 export const getTagsID = async (beanIds) => {
-  const tags = await prisma.CuffingTagBean.findMany({ 
+  const tags = await prisma.cuffingTagBean.findMany({ 
     where: { bean_id: { in: beanIds } }
   });
   return tags;
@@ -55,7 +55,7 @@ export const getTagsID = async (beanIds) => {
 
 // 태그 아이디들의 정보를 가져오는 함수
 export const getTags = async (tagIds) => {
-  const tags = await prisma.CuffingTag.findMany({ 
+  const tags = await prisma.cuffingTag.findMany({ 
     where: { cuffing_tag_id: { in: tagIds } },
     distinct: ['cuffing_tag_id']
   });
